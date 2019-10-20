f3955b7dac8e057ae6ae7af58842bbed6fdc4419
Alan Turing <freemasl.sf@gmail.com>
2019-10-20 14:08:39 -0600

    Create form for adding goals, so they list below

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]); 
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const handleSubmit = () => {
+    setCourseGoals(currentGoals => [...currentGoals, {key: Math.random().toString(), value: enteredGoal}]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hi!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+        placeholder="Course Goal" style={styles.input} onChangeText={goalInputHandler}
+        value={enteredGoal} />
+        <Button title="ADD" onPress={handleSubmit}/>
+      </View>
+      <FlatList data={courseGoals} renderItem={itemData => <View style={styles.listItem}><Text >{itemData.item.value}</Text></View>}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: "row", justifyContent: "space-between", alignItems: "center"
+  },
+  input: { 
+    borderColor: "black", 
+    borderWidth: 1, 
+    padding: 10, 
+    width: "80%" 
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1
+  }
 });
